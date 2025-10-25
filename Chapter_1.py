26af1d5647ca5e89b7ee4c592e31fabadf66eca9
D  SAI SRI HARSHIT <dsaisrih@gmail.com>
2025-10-26 02:29:36 +0530

    Add files via upload

---
+++ Chapter_1.py
@@ -0,0 +1,106 @@
+# print("D Sai Sri Harshit.\tHello","\nMy age is 19")
+# print(23+35)
+
+# name="Sai\n\n" #String
+# age=19
+
+# print("My Name is:",name)
+# print(type(name))
+
+# b=True
+# print(type(b))
+
+# a=None
+# print(a)
+
+# a=5
+# b=10
+# sum=a+b
+# print(sum)
+
+# a=2    Operaors
+# b=10
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# a=50    
+# b=100
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+# print(a>b)
+# print(a<b)
+
+# num=10  
+# print(num)
+# num+=10
+# print(num)
+# num*=10
+# print(num)
+# num-=10
+# print(num)
+# num/=10
+# print(num)
+# num%=10
+# print(num)
+# num**=10
+# print(num)
+
+# a=50
+# b=30
+# print(not False)
+# print(not(a<b))
+
+# val1=True
+# val2=False
+# print(val1 or val2)
+# print(val1 and val2)
+
+# a=2   Type Conversion
+# b=4.25
+# sum=a+b
+# a='2'
+# sum1=a+b
+# print(sum1)
+
+# a,b=1,"2"  Type Casting
+# c=int(b)
+# print(a+c)
+
+# n=input("Enter your Name :")
+# print(n) // String Value only
+
+# int("5")
+# n=int(input("Enter the Age :"))
+# print(type(n),n)
+
+
+#PP1
+# n1=int(input("Enter the First Number :"))
+# n2=int(input("Enter the Second Number :"))
+# sum=n1+n2
+# print("Sum of the Numbers :",sum)
+
+# PP2
+# side=int(input("Enter the Side of the Square :"))
+# area = side * side
+# print("Area Of the Square :",area)
+
+#PP3
+# n1=float(input("Enter the 1st Integer :"))
+# n2=float(input("Enter the 2nd Integer :"))
+# avg1=(n1+n2)/2
+# avg=(n1+n2)//2
+# print("Average of the Two numbers :",avg1)
+# print("Average of the Two numbers :",avg)
+
+#PP4
+# n1=int(input("Enter the Number 1:"))
+# n2=int(input("Enter the Number 2:"))
+# print(n1>=n2)
