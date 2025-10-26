da3b7ba39e4a5fd84f03c057d6618fc91d49bae8
D  SAI SRI HARSHIT <dsaisrih@gmail.com>
2025-10-26 21:53:50 +0530

    Add files via upload

---
+++ Chapter_2.py
@@ -0,0 +1,83 @@
+# str1 = "This is a string.\t"
+# str2='Sai Ram\n'
+# str3=""" HELLO WORLD"""
+
+# print(str2+str1+str3)
+
+# print(len(str2))
+
+# final_str=str1+" "+str2
+# print(final_str)
+
+# print(str2[3]) #Indexing
+
+# print(str1[1:6])
+# print(str1[2:len(str2)])
+# print(str1[5:])
+# print(str1[7:1])
+# print(str3[-3:-1])
+
+# str="i am a coder."
+# print(str.endswith("er."))
+# print(str.capitalize())
+# print(str.replace('a','o'))
+# print(str.find("am"))
+# print(str.count("am"))
+
+# n=input("Enter the Name : ")
+#PP1
+# print(len(n))
+#PP2
+# print(n.count("$"))
+
+# k="HELLO WORLD!!!"
+# print(k[-6:-1])
+
+# age=int(input("Enter The Age : "))
+# if (age >= 18):
+#     print("You are eligible to vote.")
+# elif(age<=18):
+#     print("You are not eligible to vote.")
+# else:
+#     print("Invalid Age.")
+
+# mark=int(input("Enter the Marks of the student :"))
+# if(mark>=90):
+#     grade="A"
+# elif(mark<90 and mark>=80):
+#     grade="B"
+# elif(mark<80 and mark>=70):
+#     grade="C"
+# else:
+#     grade="D"
+# print("Grade of the Student ->",grade)
+
+#PP1
+# num=int(input("Enter the Digit :"))
+# if(num%2==0):
+#     if(num==0):
+#         print("Neither Odd nor Even.")
+#     elif(num<=0):
+#             print("Neither Odd nor Even.")
+#     else:
+#         print("Number is Even.")
+# elif(num%2!=0):
+#     print("Number is Odd.")
+
+#PP2
+# num1=int(input("Enter Number 1 :"))
+# num2=int(input("Enter Number 2 :"))
+# num3=int(input("Enter Number 3 :"))
+# if(num1>=num2 and num1>=num3):
+#     print(num1,"is Greater than",num2,"and",num3)
+# elif(num2>=num1 and num2>=num3):
+#     print(num2,"is Greater than",num1,"and",num2)
+# else:
+#     print(num3,"is Greater than ",num1,"and",num2)
+
+#PP3
+# num=int(input("Enter the Number :"))
+# if(num%7==0):
+#     print(num,"is Multiple of 7.")
+# else:
+#     print(num,"is not multiple of 7.")
