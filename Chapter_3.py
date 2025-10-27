69c15e4e5fa80e0c5741bb2e6df6c04d3cda0d9e
D  SAI SRI HARSHIT <dsaisrih@gmail.com>
2025-10-27 15:07:32 +0530

    Add files via upload

---
+++ Chapter_3.py
@@ -0,0 +1,89 @@
+# marks=[95.4,98.4,53,98.4,58.59]
+# print(marks)
+
+# print(type(marks))
+# print(marks[0])
+# print(marks[3])
+
+# stor=[54,56.87,"SAI"]
+# print(stor)
+
+# stor[1]="SAI"
+# print(stor)
+
+# marks=[84,65,76,89,97]
+# print(marks[1:4])
+# print(marks[:4])
+# print(marks[1:])
+# print(marks[-3:-1])
+# print(marks[-1:-3])
+
+# list=[2,1,3]
+# list.append(4)
+# list.sort()
+# list.sort(reverse=True)
+# list.reverse()
+# list.insert(2,"SAI")
+# print(list)
+
+# list=["SAI","ROHIT","VAMSI","KEVIN"]
+# print(list.sort())
+# print(list)
+
+# list=[2,1,3,1]
+# # list.remove(1)
+# # list.pop(2)
+# # del list[1]
+# list.pop()
+# print(list)
+
+# tup=(2,1,3,4)
+# print(type(tup))
+# print(tup[1])
+# print(tup)
+# tuo=(1)
+
+# print(tuo)
+# print(type(tuo))
+
+# print(tup.index(1))
+# print(tup.count(2))
+
+# n=(45,45)
+# cy=n[:]
+# print(type(n))
+# print(cy)
+
+#PP1
+# list=[]
+# el1=input("Enter 1st Element :")
+# el2=input("Enter 2nd Element :")
+# el3=input("Enter 3rd Element :")
+# print(list.insert(0,el1))
+# print(list.insert(0,el2))
+# print(list.insert(0,el3))
+# print(list)
+
+#PP2
+# list1=[1,2,1]
+# list2=[2,3,4]
+# l1cp=list1.copy()
+# l2cp=list2.copy()
+# l1cp.reverse()
+# l2cp.reverse()
+# print(l1cp)
+# print(l2cp)
+# if(list1==l1cp):
+#     print("Palindrome")
+# else:
+#     print("Not a palindrome")
+
+#PP3
+# grades=("C","D","A","A","B","B","A")
+# ct=grades.count("A")
+# print(ct)
+
+#PP4
+# grades=["C","D","A","A","B","B","A"]
+# grades.sort()
+# print(grades)
